539365c4327470cc7cd5654e0d93997b23816bfe
kongming <kongming434@gmail.com>
2024-03-16 11:42:21 +0700

    read

--- Car_read.java
+++ Car_read.java
@@ -5,16 +5,19 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.json.JSONArray;  
+import org.json.JSONObject;  
 
 public class Car_read {
   public static void main(String[] args) {
     try {
       JSONArray jsonArray = readJsonArrayFromFile("Car_DataSet.json");
-      JSONObject obj=new JSONObject();
-      ArrayList<ArrayList<String>> arraylist=new ArrayList<ArrayList<String>>();
-      for (int i =0;i<jsonArray.size();i++){
-        arraylist.add()
-        
+
+      System.out.printf("%-5s %-10s %-10s %-10s %-10s %-20s %-10s\n", "ID", "Make", "Model", "Year", "Mileage",
+          "maintenace_stat", "Price");
+
+      for (int i = 0; i <= jsonArray.size(); i++) {
+        System.out.println(jsonArray.get(i));
       }
     } catch (IOException | ParseException e) {
       e.printStackTrace();
