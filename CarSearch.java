8f85f2e77eedccf54ef775a6891e7fcf3978c6bb
Pretty Git <132768132+SOYTET@users.noreply.github.com>
2024-03-16 10:16:14 +0700

    soy tet

--- CarSearch.java
+++ CarSearch.java
@@ -1,46 +1,52 @@
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
 import java.io.FileReader;
 import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class CarSearch {
     public static void main(String[] args) {
-        // Replace "path/to/Car_DataSet.json" with the actual path to your car data JSON file
-        String carDataFilePath = "./Car_DataSet.json";
+        try {
+            JSONArray jsonArray = readJsonArrayFromFile("Car_DataSet.json");
+            // System.out.println("JSON Array: " + jsonArray);
 
-        // Replace "searchModel" with the model you want to search for
-        String searchModel = "Camry";
-
-        try (FileReader reader = new FileReader(carDataFilePath)) {
-            // Parse the JSON file using Gson
-            JsonArray carsArray = JsonParser.parseReader(reader).getAsJsonObject().getAsJsonArray("cars");
-
-            // Perform the search
-            for (int i = 0; i < carsArray.size(); i++) {
-                JsonObject car = carsArray.get(i).getAsJsonObject();
-                String model = car.get("model").getAsString();
-
-                if (model.equalsIgnoreCase(searchModel)) {
-                    // Return the relevant information
-                    System.out.println("Car found:");
-                    System.out.println("Make: " + car.get("make").getAsString());
-                    System.out.println("Model: " + model);
-                    System.out.println("Year: " + car.get("year").getAsInt());
-                    System.out.println("Mileage: " + car.get("mileage").getAsInt());
-                    System.out.println("Maintenance Status: " + car.get("maintenance_status").getAsString());
-                    System.out.println("Price: " + car.get("price").getAsInt());
-
-                    return; // Stop searching after finding the first match
+            // Example usage of search function
+            JSONArray carsByMake = searchByMake(jsonArray, "Toyota");
+            if (carsByMake.size() > 0) {
+                System.out.println("Cars found:");
+                System.out.printf("%-5s %-10s %-10s\n", "ID", "Make", "Price");
+                for (Object obj : carsByMake) {
+                    JSONObject car = (JSONObject) obj;
+                    System.out.printf("%-5s %-10s %-10s\n", car.get("id"), car.get("make"), car.get("price"));
                 }
+            } else {
+                System.out.println("No cars found for the specified make.");
             }
-
-            // If no matching car is found
-            System.out.println("Car with model '" + searchModel + "' not found.");
-
-        } catch (IOException e) {
+        } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
+    }
+
+    public static JSONArray readJsonArrayFromFile(String fileName) throws IOException, ParseException {
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(new FileReader(fileName));
+        JSONObject jsonObj = (JSONObject) obj;
+
+        return (JSONArray) jsonObj.get("Cars");
+    }
+
+    public static JSONArray searchByMake(JSONArray jsonArray, String makeName) {
+        JSONArray result = new JSONArray();
+
+        for (Object obj : jsonArray) {
+            JSONObject car = (JSONObject) obj;
+            String make = (String) car.get("make");
+            if (make.equalsIgnoreCase(makeName)) {
+                result.add(car);
+            }
+        }
+
+        return result;
     }
 }
