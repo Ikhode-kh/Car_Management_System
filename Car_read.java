ecce6c1a070055f6fa1b11b79d49e8bd1116f1c4
kongming <kongming434@gmail.com>
2024-03-19 14:44:11 +0700

    Update Car_read.java

--- Car_read.java
+++ Car_read.java
@@ -1,22 +1,22 @@
-
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import java.util.*; 
+import org.json.*; 
 
 public class Car_read {
   public static void main(String[] args) {
     try {
       JSONArray jsonArray = readJsonArrayFromFile("Car_DataSet.json");
       System.out.printf("%-5s %-10s %-10s %-10s %-10s %-20s %-10s\n", "ID", "Make", "Model", "Year", "Mileage",
-          "maintenace_stat", "Price");
-          for (Object obj : jsonArray) {
-            JSONObject car = (JSONObject) obj;
-            System.out.printf("%-5s %-10s %-10s %-10s %-10s %-20s %-10s\n", car.get("id"), car.get("make"), car.get("model"),car.get("year"),car.get("mileage"),car.get( "maintenace_stat"),car.get("price"));
-        }
+          "Maintenance", "Price");
+      List<String> value=new ArrayList<String>();
+      for (int i=0;i<jsonArray.Length)
 
     } catch (IOException | ParseException e) {
       e.printStackTrace();
@@ -25,15 +25,8 @@ public class Car_read {
 
   public static JSONArray readJsonArrayFromFile(String fileName) throws IOException, ParseException {
     JSONParser parser = new JSONParser();
-    Object obj = parser.parse(new FileReader(fileName));
-    JSONObject jsonObj = (JSONObject) obj;
-
-    JSONArray jsonArray = new JSONArray();
-    for (Object key : jsonObj.keySet()) {
-      String diagramKey = (String) key;
-      jsonArray.add(jsonObj.get(diagramKey));
+    try (FileReader reader = new FileReader(fileName)) {
+      return (JSONArray) parser.parse(reader);
     }
-    return jsonArray;
   }
 }
-
