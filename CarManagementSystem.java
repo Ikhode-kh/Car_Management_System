6b1182802d2dc806db7433873aec0666d51d9c24
Pretty Git <132768132+SOYTET@users.noreply.github.com>
2024-03-21 09:25:06 +0700

    sd

--- CarManagementSystem.java
+++ CarManagementSystem.java
@@ -74,53 +74,55 @@ class carManagementSystem {
         // Car_read.readJsonArrayFromFile("Car_DataStorage.json");
     }
 
+    @SuppressWarnings("unchecked")
     private void addCar() {
         // AddCar.main(null);
 
-        Scanner ent = new Scanner(System.in);
+        try (Scanner ent = new Scanner(System.in)) {
+            System.out.println("Enter Car ID: ");
+            int id = ent.nextInt();
+            ent.nextLine();
+            System.out.println("Enter Car Make: ");
+            String make = ent.nextLine();
 
-        System.out.println("Enter Car ID: ");
-        int id = ent.nextInt();
-        ent.nextLine();
-        System.out.println("Enter Car Make: ");
-        String make = ent.nextLine();
+            System.out.println("Enter Car model: ");
+            String model = ent.nextLine();
 
-        System.out.println("Enter Car model: ");
-        String model = ent.nextLine();
+            System.out.println("Enter Car year: ");
+            int year = ent.nextInt();
+            System.out.println("Enter Car mileage: ");
+            int mileage = ent.nextInt();
+            ent.nextLine();
+            System.out.println("Enter Car Maintenace status: ");
+            String maintenace_stat = ent.nextLine();
 
-        System.out.println("Enter Car year: ");
-        int year = ent.nextInt();
-        System.out.println("Enter Car mileage: ");
-        int mileage = ent.nextInt();
-        ent.nextLine();
-        System.out.println("Enter Car Maintenace status: ");
-        String maintenace_stat = ent.nextLine();
+            System.out.println("Enter Car Price: ");
+            int price = ent.nextInt();
 
-        System.out.println("Enter Car Price: ");
-        int price = ent.nextInt();
-
-        AddCar newCar = new AddCar(id, make, model, year, mileage, maintenace_stat, price);
-        JSONObject jsonObj = new JSONObject();
-        jsonObj.put("Price", newCar.price);
-        jsonObj.put("Maintenace_stat", newCar.maintenace_stat);
-        jsonObj.put("Mileage", newCar.mileage);
-        jsonObj.put("Year", newCar.year);
-        jsonObj.put("Model", newCar.model);
-        jsonObj.put("Make", newCar.make);
-        jsonObj.put("Id", newCar.id);
-        String filepath = "Car_DataStorage.json";
-        try (Writer writeData = new FileWriter(filepath, true)) {
-            writeData.write(jsonObj.toJSONString() + "\n"); // Append new car data to the file
-            System.out.println("Successfully added new car to JSON file.");
-        } catch (IOException e) {
-            e.printStackTrace();
+            AddCar newCar = new AddCar(id, make, model, year, mileage, maintenace_stat, price);
+            JSONObject jsonObj = new JSONObject();
+            jsonObj.put("Price", newCar.price);
+            jsonObj.put("Maintenace_stat", newCar.maintenace_stat);
+            jsonObj.put("Mileage", newCar.mileage);
+            jsonObj.put("Year", newCar.year);
+            jsonObj.put("Model", newCar.model);
+            jsonObj.put("Make", newCar.make);
+            jsonObj.put("Id", newCar.id);
+            String filepath = "Car_DataStorage.json";
+            try (Writer writeData = new FileWriter(filepath, true)) {
+                writeData.write(jsonObj.toJSONString() + "\n"); // Append new car data to the file
+                System.out.println("Successfully added new car to JSON file.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
+
     private void deleteCar() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter ID of the car to delete: ");
         int carId = scanner.nextInt();
         scanner.close();
-        CarDeletion.deleteCar("Car_DataStorage.json", carId); 
+        CarDeletion.deleteCar("Car_DataStorage.json", carId);
     }
 }
